58d48b5c0d1762fe61059328a63bc65f58829171
Rae <RaeTStone@gmail.com>
2021-12-30 12:45:47 -0500

    test

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ app.set("view engine", "ejs");
 app.use('/public', express.static('public'))
 
 app.get("/", (req, res) => {
-    res.send("hello");
-    //res.render("home");
+    res.render("home");
 })
 
 app.get("/room/15", (req, res) => {
