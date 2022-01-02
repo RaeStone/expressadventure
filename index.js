bad9449cb567f4dac7cc3d261410f2027ddd1a00
Rae <RaeTStone@gmail.com>
2022-01-02 15:04:12 -0500

    finished

--- index.js
+++ index.js
@@ -6,6 +6,22 @@ let item1 = false;
 let item2 = false;
 let item3 = false;
 
+let rm1 = false;
+let rm2 = false;
+let rm3 = false;
+let rm4 = false;
+let rm5 = false;
+let rm6 = false;
+let rm7 = false;
+let rm8 = false;
+let rm9 = false;
+let rm10 = false;
+let rm11 = false;
+let rm12 = false;
+let rm13 = false;
+let rm14 = false;
+let rm15 = false;
+
 app.set("view engine", "ejs");
 app.use('/public', express.static('public'))
 
@@ -13,13 +29,95 @@ app.get("/", (req, res) => {
     res.render("home");
 })
 
-app.get("/room/15", (req, res) => {
-    res.render("15", {item1, item2, item3});
+app.get("/map", (req, res) => {
+    res.render("map",{rm1, rm2, rm3, rm4, rm5, rm6, rm7, rm8, rm9, rm10, rm11, rm12, rm13, rm14, rm15});
 })
+
+/* app.get("/room/15", (req, res) => {
+    res.render("15", {item1, item2, item3});
+    rm15 = true;
+})
+
+app.get("/room/1", (req, res) => {
+    res.render("1", {item1});
+    rm1 = true;
+})
+
+app.get("/room/3", (req, res) => {
+    res.render("3", {item2});
+    rm3 = true;
+})
+
+app.get("/room/13", (req, res) => {
+    res.render("13", {item3});
+    rm13 = true;
+}) */
 
 app.get("/room/:id", (req, res) => {
     const id = req.params['id'];
-    res.render(id);
+
+    switch (id) {
+        case "1":
+            rm1 = true;
+            res.render(id, {item1});
+            break;
+        case "2":
+            rm2 = true;
+            res.render(id);
+            break;
+        case "3":
+            rm3 = true;
+            res.render(id, {item2});
+            break;
+        case "4":
+            rm4 = true;
+            res.render(id);
+            break;
+        case "5":
+            rm5 = true;
+            res.render(id);
+            break;
+        case "6":
+            rm6 = true;
+            res.render(id);
+            break;
+        case "7":
+            rm7 = true;
+            res.render(id);
+            break;
+        case "8":
+            rm8 = true;
+            res.render(id);
+            break;
+        case "9":
+            rm9 = true;
+            res.render(id);
+            break;
+        case "10":
+            rm10 = true;
+            res.render(id);
+            break;
+        case "11":
+            rm11 = true;
+            res.render(id);
+            break;
+        case "12":
+            rm12 = true;
+            res.render(id);
+            break;
+        case "13":
+            rm13 = true;
+            res.render(id, {item3});
+            break;
+        case "14":
+            rm14 = true;
+            res.render(id);
+            break;
+        case "15":
+            rm15 = true;
+            res.render(id, {item1, item2, item3});
+            break;
+    }
 })
 
 app.get("/tada", (req, res) => {
